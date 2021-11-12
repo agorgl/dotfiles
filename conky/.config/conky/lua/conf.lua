867ede4a286a3c5eb94a27d0c9f28ae04b1b99b8
Agorgianitis Loukas <agorglouk@gmail.com>
2021-11-12 19:35:01 +0200

    Config color references

--- conky/.config/conky/lua/conf.lua
+++ conky/.config/conky/lua/conf.lua
@@ -10,10 +10,17 @@ local color_table = {
 }
 
 --
+-- Read color reference for given alias
+--
+function color_ref(r)
+    return command('colref ' .. r)
+end
+
+--
 -- Read colors from environment to local variables
 --
 function config_load_colors()
-    local u = { main = 'color4', altn = 'color8' }
+    local u = { main = color_ref('main'), altn = color_ref('altn') }
     for k, v in pairs(u) do
         color_table[k] = xcolor(v)
     end
